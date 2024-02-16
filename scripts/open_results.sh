cb2354fa10f56b60f8f62963d7c721adea7fdbca
Andreas Prouza <andreas@prouza.at>
2024-02-16 06:14:35 +0100

    Integrate in scripts; Output MD file

--- scripts/open_results.sh
+++ scripts/open_results.sh
@@ -3,6 +3,6 @@
 # Import global config
 source $(dirname $(realpath "$0"))/init.sh
 
-obi/venv/bin/python obi/main.py -a create -p $WORKSPACE_FOLDER
+obi/venv/bin/python obi/main.py -a open_result -p $WORKSPACE_FOLDER
 
 echo -e "$COLOR_GREEN \n finished creation of build script $COLOR_END"
