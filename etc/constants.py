4e724ad164b7234a95c5f8b64d115f88ae34c832
Hansi Test <andreas-test@prouza.at>
2024-06-06 14:58:27 +0200

    Add support for individual settings

--- etc/constants.py
+++ etc/constants.py
@@ -1,3 +1,4 @@
 CONFIG_TOML = 'etc/app-config.toml'
+SOURCE_CONFIG_TOML = 'etc/source-config.toml'
 DEPENDEND_OBJECT_LIST = 'tmp/dependend-object-list.json'
 CHANGED_OBJECT_LIST = 'tmp/changed-object-list.json'
