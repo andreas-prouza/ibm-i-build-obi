53fffd3489ac7d6dc629a45d600c613314d24c76
Andreas Prouza <andreas@prouza.at>
2024-11-15 14:15:54 +0100

    Merge

---
+++ .obi/etc/constants.py
@@ -0,0 +1,6 @@
+CONFIG_TOML = '.obi/etc/app-config.toml'
+CONFIG_USER_TOML = '.obi/etc/.user-app-config.toml'
+SOURCE_CONFIG_TOML = '.obi/etc/source-config.toml'
+DEPENDEND_OBJECT_LIST = '.obi/tmp/dependend-object-list.json'
+CHANGED_OBJECT_LIST = '.obi/tmp/changed-object-list.json'
+UPDATE_OBJECT_LIST = False
