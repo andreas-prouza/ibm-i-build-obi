7c71c3008e9d3ff02282304ab02a333f6363112b
Andreas Prouza <andreas@prouza.at>
2025-08-08 16:47:26 +0200

    added constants

--- .obi/etc/constants.py
+++ .obi/etc/constants.py
@@ -3,4 +3,6 @@ CONFIG_USER_TOML = '.obi/etc/.user-app-config.toml'
 SOURCE_CONFIG_TOML = '.obi/etc/source-config.toml'
 DEPENDEND_OBJECT_LIST = '.obi/tmp/dependend-object-list.json'
 CHANGED_OBJECT_LIST = '.obi/tmp/changed-object-list.json'
+EXTENDED_SOURCE_PROCESS_CONFIG_TOML = '.obi/etc/extended-source-processing-config.toml'
+ESP_SCRIPT_FOLDER='.obi/etc/scripts'
 UPDATE_OBJECT_LIST = False
